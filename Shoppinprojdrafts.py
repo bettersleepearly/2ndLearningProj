7591ec5fb68fcff523579d666856bcee471d39a9
Jack Tran (bettersleepearly) <anhviet3326@gmail.com>
2020-06-08 06:15:48 -0400

    Add files via upload

---
+++ Shoppinprojdrafts.py
@@ -0,0 +1,27 @@
+print('\n1.Dinghy Tiger: 139'+currency+'\n(No Picture LMAO)')
+            print('\n2.Dinghy FG Watercolor: 149'+currency+'\n(No Picture LMAO)')
+            print('\n3.Dinghy Crown Peak: 139'+currency+'\n(No Picture LMAO)')
+            print('\n4.Dinghy BK: 139'+currency+'\n(No Picture LMAO)')
+            print('\n5.Gordito Pantera: 169'+currency+'\n(No Picture LMAO)')
+            print('\n6.Ditchlife: 139'+currency+'\n(No Picture LMAO)')
+            print('\n7.Wrecktangle Lighthouse: 139'+currency+'\n(No Picture LMAO)')
+            print('\n8.Tugboat Captain: 139'+currency+'\n(No Picture LMAO)')
+            print('\n9.Tugboat Chill Cat: 139'+currency+'\n(No Picture LMAO)')
+            print('\n10.Tugboat Midnight Snek: 149'+currency+'\n(No Picture LMAO)')
+            print('\n11.Dinghy Arctic Fox: 134'+currency+'\n(No Picture LMAO)')
+            print('\n12.Mighty Mite: 129'+currency+'\n(No Picture LMAO)')
+
+            pricecruiser = {'cruiser1':139 +currency,
+            'cruiser2':149 +currency,
+            'cruiser3':139 +currency,
+            'cruiser4':139 +currency,
+            'cruiser5':169 +currency,
+            'cruiser6':139 +currency,
+            'cruiser7':139 +currency,
+            'cruiser8':139 +currency,
+            'cruiser9':139 +currency,
+            'cruiser10':149 +currency,
+            'cruiser11':134 +currency,
+            'cruiser12':129 + currency}
+            
+            print(pricecruiser)
